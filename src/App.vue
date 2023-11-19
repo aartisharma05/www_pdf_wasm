7f8b3d8e5b954e20a41776ad711a285939822984
Vijay <vijay@iavian.com>
2023-11-19 10:59:30 +0530

    Initial

--- src/App.vue
+++ src/App.vue
@@ -46,30 +46,25 @@ pageStore.setWorker(worker);
       <!-- Mobile menu, show/hide based on menu open state.-->
       <div class="md:hidden" role="dialog" aria-modal="true">
         <!-- Background backdrop, show/hide based on slide-over state.  -->
-        <div class="fixed inset-0 z-10"></div>
 
         <div
           class="fixed inset-y-0 right-0 z-10 w-2/3 overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 mx-auto">
           <div class="flex items-center justify-between">
-            <RouterLink to="/" class="-m-1.5 p-1.5">
-              <span class="sr-only">PDF Extract</span>
-              <img src="/assets/img/logo.svg" />
-            </RouterLink>
-            <div class="flex items-center">
-              <RouterLink to="/register">
-                <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold p-2 mx-2 rounded float-right">
-                  Register
-                </button>
-              </RouterLink>
 
-              <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-                <span class="sr-only">Close menu</span>
-                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                  aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
+            <RouterLink to="/register">
+              <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold p-2 mx-2 rounded float-right">
+                Register
               </button>
-            </div>
+            </RouterLink>
+
+            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+              <span class="sr-only">Close menu</span>
+              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+            
           </div>
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
